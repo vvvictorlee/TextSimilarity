506a446133210686f351a26c323bcea5b296b54c
Payton_戴鹏程 <dpc11@qq.com>
2017-04-13 13:52:59 +0800

    更新细节

--- TextSimilarity-VSM-Gensim.py
+++ TextSimilarity-VSM-Gensim.py
@@ -13,9 +13,9 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 # 读取文件并分词，同时过滤标点符号
 count = 0
 start_time = time.time()
+indexs = []  # 编号列表
+documents = []  # 文档分词列表
 with open('199801_clear.txt', 'r', encoding='GBK') as f_in:
-    indexs = []   # 编号列表
-    documents = []   # 文档分词列表
     index = ''   # 文档编号
     doc = []   # 文档分词
     for line in f_in:
@@ -30,12 +30,13 @@ with open('199801_clear.txt', 'r', encoding='GBK') as f_in:
                 if doc:
                     documents.append(doc)
                 doc = []
-            for index1 in range(1, len(words)):
+            for i in range(1, len(words)):
 
                 # 学习其他同学过滤掉无意义的标点符号和助词等
-                if '/w' not in words[index1] and '/y' not in words[index1] and \
-                        '/u' not in words[index1] and '/c' not in words[index1]:
-                    doc.append(words[index1])
+                word = words[i]
+                if '/w' not in word and '/y' not in word and '/u' not in word \
+                        and '/c' not in word:
+                    doc.append(word)
     documents.append(doc)
 
 # 提取出现频数大于 1 的关键词作为词袋，以此为基础将文档向量化并使用 TF-IDF 作为词的权重
@@ -56,8 +57,8 @@ corpus_tfidf = corpora.MmCorpus('tmp/corpus_tfidf.mm')
 
 # 构建文档相似度矩阵索引用于查询，再使用文档列表本身进行相似度查询（默认使用 Cosine）
 index = similarities.SparseMatrixSimilarity(corpus_tfidf)
-with open('result.txt', 'w') as f_out:
-    for sim in index[corpus_tfidf]:
-        f_out.write(','.join(map(str, sim)) + '\n')
+with open('result.csv', 'w') as f_out:
+    for sims in index[corpus_tfidf]:
+        f_out.write(','.join(map(str, sims)) + '\n')
 
 print("总共耗时（秒）：" + str(time.time() - start_time))
